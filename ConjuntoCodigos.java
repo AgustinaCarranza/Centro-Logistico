648ac64c4df079c905e15c28eb33ff4dcd8537f4
AgustinaCarranza <agcarranza@uade.edu.ar>
2026-06-03 11:01:18 -0300

    Add files via upload

--- ConjuntoCodigos.java
+++ ConjuntoCodigos.java
@@ -1,34 +1,34 @@
 public class ConjuntoCodigos {
-    private int[] codigos;
-    private int cantidad;
+    private int[] codigos; //guardamos los codigos
+    private int cantidad;// la cantidad de codigos
 
     public ConjuntoCodigos(int capacidad){
-        codigos = new int[capacidad];
-        cantidad = 0;
+        codigos = new int[capacidad];//iniciamos el arreglo
+        cantidad = 0;// no tiene codigos
     }
     public void insertar(int codigo){
-        if (cantidad < codigos.length){
-            codigos[cantidad] = codigo;
+        if (cantidad < codigos.length){//verificamos el espacio
+            codigos[cantidad] = codigo;//si esta libre lo agregamos
             cantidad++;
         }
     }
     public void eliminar(int codigo){
-        for (int i = 0; i < cantidad; i++){
-            if (codigos[i] == codigo){
-                for (int j = i; j < cantidad - 1; j++){
+        for (int i = 0; i < cantidad; i++){//recorremos el codigo
+            if (codigos[i] == codigo){//cuando lo encontramos
+                for (int j = i; j < cantidad - 1; j++){//movemos los otros elementos hacia atras
                     codigos[j] = codigos[j + 1];
                 }
                 cantidad--;
-                break;
+                break;//salimos del bucle
             }
         }
     }
     public boolean pertenece(int codigo){
-        for (int i = 0; i < cantidad; i++){
-            if (codigos[i] == codigo){
-                return true;
+        for (int i = 0; i < cantidad; i++){//recoremos los codidos
+            if (codigos[i] == codigo){//si lo encontramos
+                return true;//devolvemos true porque lo encontramos
             }
         }
-        return false;
+        return false;// si no devuelve falso
     }
 }
