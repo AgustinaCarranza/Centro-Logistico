b24ca8f10bed52cccd81c44449ad9a8f2894deec
AgustinaCarranza <agcarranza@uade.edu.ar>
2026-06-08 15:18:10 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Producto p1 = new Producto("Leche",55555,89);
+        Producto p2 = new Producto("Arroz", 90988980, 123);
 
+        //guarda los datos en el diccionario
+        DiccionarioProductos diccionario = new DiccionarioProductos(10);
+        diccionario.insertar(p1.getCodigo(),p1);
+        diccionario.insertar(p2.getCodigo(), p2);
+
+        //entrada y salida
         MovimientoInventario entrada = new MovimientoInventario(p1,6,"entrada");
         entrada.movimiento();
         System.out.println(entrada.mostrarDetalles());
@@ -9,6 +16,10 @@ public class Main {
         MovimientoInventario salida = new MovimientoInventario(p1,20,"salida");
         salida.movimiento();
         System.out.println(salida.mostrarDetalles());
+
+        //nos permite buscar por codigo un producto
+        Producto buscado = diccionario.recuperar(90988980);
+        System.out.println("Encontrado " + buscado.getNombre());
     }
 
 }
